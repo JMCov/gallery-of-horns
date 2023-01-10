1c0d0d59682d3e0f33b1c5573f6448ecbc10d569
Jordan Covington <Atlantafan1087@gmail.com>
2023-01-10 12:52:17 -0800

    Lab 2 complete

--- README.md
+++ README.md
@@ -69,3 +69,37 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
 # gallery-of-horns
+
+Number and name of feature: #1 Display images
+
+Estimate of time needed to complete: 15 minutes
+
+Start time: 12:00PM
+
+Finish time: 12:25PM
+
+Actual time needed to complete: 25min
+
+
+
+Number and name of feature: #2 Allow Users to Favorite Individual Beasts
+
+Estimate of time needed to complete: 15 minutes
+
+Start time: 12:25PM
+
+Finish time: 12:35PM
+
+Actual time needed to complete: 10min
+
+
+
+Number and name of feature: #3 Bootstrap
+
+Estimate of time needed to complete: 30 minutes
+
+Start time: 12:35PM
+
+Finish time: 12:45PM
+
+Actual time needed to complete: 10min
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,14 +1,43 @@
 import React from 'react';
+import './HornedBeast.css'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-class HornedBeast extends React.Component{
+class HornedBeast extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites: 0,
+    }
+  }
+
+  handleFav = () => {
+    this.setState({
+      favorites: this.state.favorites + 1
+    });
+  }
+
   render() {
     return (
       <>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.img} alt={this.props.description} title={this.props.title}/>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={this.props.image_url} alt={this.props.description} />
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>{this.props.description}</Card.Text>
+            <Card.Text>❤️{this.state.favorites} Favorites!</Card.Text>
+            <Button onClick={this.handleFav} variant="outline-danger">Click to Favorite</Button>
+          </Card.Body>
+        </Card>
+
+
+        {/* <h2>{this.props.title}</h2>
+        <img src={this.props.image_url} alt={this.props.description} title={this.props.title}/>
         <p>{this.props.description}</p>
+        <p onClick={this.handleFav}>Favorite!</p> */}
       </>
-      )
+    )
   }
 }
 
