4627a857afa0691b32fef94d517852f16f1d21b2
Jordan Covington <Atlantafan1087@gmail.com>
2023-01-11 13:47:39 -0800

    Modified to use Modals

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,21 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
-import data from './data/data.json'
 
 class Main extends React.Component {
   render() {
     return (
       <>
         <main>
-          {data.map((hornedbeast) => {
-            return <HornedBeast title={hornedbeast.title} image_url={hornedbeast.image_url} description={hornedbeast.description} key={hornedbeast._id} />
-          })}
+          {this.props.data.map((hornedbeast) => {
+            return (
+            <HornedBeast 
+              title={hornedbeast.title} 
+              image_url={hornedbeast.image_url} 
+              description={hornedbeast.description} 
+              key={hornedbeast._id} 
+              handleOpenModal={this.props.handleOpenModal}
+              />
+          )})}
         </main>
       </>
     )
